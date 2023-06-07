c47302330acf9ada5d2c55c6df1638cb9b5b49b4
genokamura <gen.okamura@tokyo.supersoftware.co.jp>
2023-06-07 12:40:34 +0900

    fix: import order

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,15 @@
 
 namespace App\Models;
 
-use App\Notifications\PasswordResetNotification;
 use App\Notifications\FullRegistrationNotification;
+use App\Notifications\PasswordResetNotification;
 use App\Notifications\VerifyEmail;
-use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
